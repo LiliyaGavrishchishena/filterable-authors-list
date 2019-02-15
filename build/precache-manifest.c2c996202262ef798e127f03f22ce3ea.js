e6ddb4e6101795dac44edcd3639397602a3d8802
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-02-15 12:59:55 +0200

    filterable-authors-list

---
+++ build/precache-manifest.c2c996202262ef798e127f03f22ce3ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "98642d3cceaaf0e9bbe88916d5c2f82e",
+    "url": "/static/media/3rd.98642d3c.svg"
+  },
+  {
+    "revision": "74629788d1ed8d1336b2899fe54d2db0",
+    "url": "/static/media/2nd.74629788.svg"
+  },
+  {
+    "revision": "86c17a7097b302da04a1ab845ebec5ee",
+    "url": "/static/media/1st.86c17a70.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bfe96151c7c7aeb400b2",
+    "url": "/static/js/main.bfe96151.chunk.js"
+  },
+  {
+    "revision": "73a29844f70fd7c3c838",
+    "url": "/static/js/2.73a29844.chunk.js"
+  },
+  {
+    "revision": "bfe96151c7c7aeb400b2",
+    "url": "/static/css/main.c604e230.chunk.css"
+  },
+  {
+    "revision": "31af4f7d1d43c73636b909ed6e859e11",
+    "url": "/index.html"
+  }
+];
